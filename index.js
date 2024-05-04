4caca34bea2ef7d0c9508919b5f1be22598c3a10
huzaifa khan <hk4899541@gmail.com>
2024-05-04 23:28:36 +0500

    initial files uploaded

---
+++ index.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+const randomnumber = Math.floor(Math.random() * 10);
+const answers = await inquirer.prompt([
+    {
+        name: "userGuessednumber",
+        type: "number",
+        message: "please guess a number between 1-10 : ",
+    },
+]);
+if (answers.userGuessednumber === randomnumber) {
+    console.log("Conratulation you guessed the right number!");
+}
+else {
+    console.log(" you guessed the wrong  number!");
+}
+console.log(answers);
